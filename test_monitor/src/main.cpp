a35ce57bbe21acc7fcf15d79053a86a554affb95
super <super@1345888144@qq.com>
2023-07-02 10:31:28 +0800

    [monitor] add thread

--- test_monitor/src/main.cpp
+++ test_monitor/src/main.cpp
@@ -8,31 +8,35 @@
 #include "monitor/cpu_softirq_monitor.h"
 #include "monitor/cpu_stat_monitor.h"
 #include "monitor/mem_monitor.h"
+#include "monitor/monitor_inter.h"
 #include "monitor/net_monitor.h"
 
-#include "monitor/monitor_inter.h"
 #include "monitor_info.grpc.pb.h"
 #include "monitor_info.pb.h"
 
 int main() {
-  std::vector<monitor::MonitorInter*> runners_;
-  runners_.push_back(new monitor::CpuSoftIrqMonitor());
-  runners_.push_back(new monitor::CpuLoadMonitor());
-  runners_.push_back(new monitor::CpuStatMonitor());
-  runners_.push_back(new monitor::MemMonitor());
-  runners_.push_back(new monitor::NetMonitor());
+  std::vector<std::shared_ptr<monitor::MonitorInter>> runners_;
+  runners_.emplace_back(new monitor::CpuSoftIrqMonitor());
+  runners_.emplace_back(new monitor::CpuLoadMonitor());
+  runners_.emplace_back(new monitor::CpuStatMonitor());
+  runners_.emplace_back(new monitor::MemMonitor());
+  runners_.emplace_back(new monitor::NetMonitor());
 
   monitor::RpcClient rpc_client_;
 
-  while (true) {
-    monitor::proto::MonitorInfo monitor_info;
-    for (auto& runner : runners_) {
-      runner->UpdateOnce(&monitor_info);
+  std::unique_ptr<std::thread> thread_ = nullptr;
+  thread_ = std::make_unique<std::thread>([&]() {
+    while (true) {
+      monitor::proto::MonitorInfo monitor_info;
+      for (auto& runner : runners_) {
+        runner->UpdateOnce(&monitor_info);
+      }
+
+      rpc_client_.SetMonitorInfo(monitor_info);
+      std::this_thread::sleep_for(std::chrono::seconds(3));
     }
+  });
 
-    rpc_client_.SetMonitorInfo(monitor_info);
-    std::this_thread::sleep_for(std::chrono::seconds(3));
-  }
-
+  thread_->join();
   return 0;
 }
